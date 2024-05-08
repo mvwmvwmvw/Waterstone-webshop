15a198926f19d5b9cbc52b06edf86e0a43d4e940
Diana Pronina <diana.pronina@tptlive.ee>
2024-05-08 15:59:48 +0300

    add

--- tests/search.test.js
+++ tests/search.test.js
@@ -1,4 +1,5 @@
-const {Builder, Browser} = require('selenium-webdriver');
+const {By, Builder, Browser} = require('selenium-webdriver');
+require('chromedriver');
 describe('Search products by keywords', () => {
 
     test('Test Open Web Page', async () => {
@@ -12,12 +13,26 @@ describe('Search products by keywords', () => {
     test('More than one product', async () => {
         driver = await new Builder().forBrowser(Browser.CHROME).build();
         await driver.get('https://www.waterstones.com/');
+        await driver.manage().setTimeouts({implicit: 5000});
+        let rejectButton = await driver.findElement(By.id('onetrust-reject-all-handler'));
+        await rejectButton.click();
         let searchBox = await driver.findElement(By.name('term'));
-        let submitButton = await driver.findElement(By.type('submit'));
+        let submitButton = await driver.findElement(By.className('input-search-button icon'));
         await searchBox.sendKeys('harry potter');
         await submitButton.click();
-        let 
-        expect().toBe();
+        let searchResult = await driver.findElement(By.className('search-result-tab-all')).then(function(){
+            return true;
+        });
+        expect(searchResult).toBe(true);
+        await driver.quit();
+    })
+
+    test('Test Products Presented have searched keyword in it', async () => {
+        driver = await new Builder().forBrowser(Browser.CHROME).build();
+        await driver.get('https://www.waterstones.com/');
+        await driver.manage().setTimeouts({implicit: 5000});
+        let rejectButton = await driver.findElement(By.id('onetrust-reject-all-handler'));
+        await rejectButton.click();
 
         await driver.quit();
     })
